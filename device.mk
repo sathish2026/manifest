a3995ec54e1c252fcb002e98cbecbc168e27a6c3
sathish2026 <txt2sathish@gmail.com>
2017-11-26 14:23:01 +0530

    Mido:Build Hidl manager

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
+# Hidl
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0-java
+
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
