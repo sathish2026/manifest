rohitporoli/build

Use Google Pixel Audio Files [1/2]
* Cardinal edits:

 move files to vendor

target/product/full_base.mk
@@ -19,6 +19,11 @@
 # build of the emulator, but all those aspects can be overridden
 # in inherited configurations.
 
+ # Additional settings used in all AOSP builds
+ PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Tethys.ogg
+ 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
     OpenWnn \
@@ -46,7 +51,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, vendor/cardinal/prebuilt/common/media/MaterialSounds/GoogleAudio.mk) 
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)

target/product/sdk_base.mk
@@ -88,9 +88,6 @@ PRODUCT_PACKAGE_OVERLAYS := development/sdk_overlay
 PRODUCT_COPY_FILES := \
 	device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
-	frameworks/base/data/sounds/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	frameworks/base/data/sounds/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-	frameworks/base/data/sounds/effects/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
 	device/generic/goldfish/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -106,7 +103,7 @@ PRODUCT_COPY_FILES := \
 
 include $(SRC_TARGET_DIR)/product/emulator.mk
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, vendor/cardinal/prebuilt/common/media/MaterialSounds/GoogleAudio.mk)
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/dancing-script/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/carrois-gothic-sc/fonts.mk)

sathish2026/manifest

Change-Id: If7813f5d486d7ea61a35948a8ecfd4eaeae83566

target/product/full_base.mk
@@ -37,16 +37,16 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     PhotoTable
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
-
+ # Additional settings used in all AOSP builds
+ PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Tethys.ogg
+ 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, vendor/citrus/prebuilt/common/media/MaterialSounds/GoogleAudio.mk) 
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)

target/product/sdk_base.mk
@@ -88,9 +88,6 @@ PRODUCT_PACKAGE_OVERLAYS := development/sdk_overlay
 PRODUCT_COPY_FILES := \
 	device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
-	frameworks/base/data/sounds/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	frameworks/base/data/sounds/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-	frameworks/base/data/sounds/effects/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
 	device/generic/goldfish/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -107,7 +104,7 @@ PRODUCT_COPY_FILES := \
 
 include $(SRC_TARGET_DIR)/product/emulator.mk
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, vendor/citrus/prebuilt/common/media/MaterialSounds/GoogleAudio.mk)
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/dancing-script/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/carrois-gothic-sc/fonts.mk)