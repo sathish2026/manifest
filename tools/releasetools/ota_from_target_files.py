Citrus-CAF/build

Show useful information after flashing
* Credits to zephiK and Chroma for the original code
* Fixed Spacing

used krexus-caf/build@5c602f2 commit from krexus,

* show citrus flavor
* show device next to Citrus-CAF
* Show CAF branch

tools/releasetools/ota_from_target_files.py
@@ -683,6 +683,20 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    build = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
+    date = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    flavour = GetBuildProp("ro.citrus.flavour", OPTIONS.info_dict)
+    caf = GetBuildProp("ro.caf.branch", OPTIONS.info_dict)
+
+    script.Print("***********************************************");
+    script.Print("           Citrus-CAF for %s"%(model));
+    script.Print("	   Flavour: %s"%(flavour));
+    script.Print("     CAF Branch: %s"%(caf));
+    script.Print("     AOSP Version: %s"%(build));
+    script.Print("     Compiled: %s"%(date));
+    script.Print("***********************************************");
+
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   system_items = ItemSet("system", "META/filesystem_config.txt")

sathish2026/manifest

Change-Id: Ie12b087817e456b8e43f58c1532a04ac703cd21f

tools/releasetools/ota_from_target_files.py
@@ -483,6 +483,20 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    build = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
+    date = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    flavour = GetBuildProp("ro.citrus.flavour", OPTIONS.info_dict)
+    caf = GetBuildProp("ro.caf.branch", OPTIONS.info_dict)
+
+    script.Print("***********************************************");
+    script.Print("           Citrus-CAF for %s"%(model));
+    script.Print("     Flavour: %s"%(flavour));
+    script.Print("     CAF Revision: %s"%(caf));
+    script.Print("     AOSP Version: %s"%(build));
+    script.Print("     Compiled: %s"%(date));
+    script.Print("***********************************************");
+
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   script.ShowProgress(system_progress, 0)