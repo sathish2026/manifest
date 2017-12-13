Citrus-CAF/build

build: Fix support for devices that don't have ro.product.model

tools/releasetools/ota_from_target_files.py
@@ -685,13 +685,23 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
     build = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
     date = GetBuildProp("ro.build.date", OPTIONS.info_dict)
-    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     flavour = GetBuildProp("ro.citrus.flavour", OPTIONS.info_dict)
     caf = GetBuildProp("ro.caf.branch", OPTIONS.info_dict)
 
+  if GetBuildProp("ro.product.model", OPTIONS.info_dict) is not None:
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     script.Print("***********************************************");
     script.Print("           Citrus-CAF for %s"%(model));
-    script.Print("	   Flavour: %s"%(flavour));
+    script.Print("     Flavour: %s"%(flavour));
+    script.Print("     CAF Branch: %s"%(caf));
+    script.Print("     AOSP Version: %s"%(build));
+    script.Print("     Compiled: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = GetBuildProp("ro.product.name", OPTIONS.info_dict)
+    script.Print("***********************************************");
+    script.Print("           Citrus-CAF for %s"%(name));
+    script.Print("     Flavour: %s"%(flavour));
     script.Print("     CAF Branch: %s"%(caf));
     script.Print("     AOSP Version: %s"%(build));
     script.Print("     Compiled: %s"%(date));
@@ -842,8 +852,8 @@ def GetBuildProp(prop, info_dict):
   try:
     return info_dict.get("build.prop", {})[prop]
   except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
-
+    print ("WARNING: could not find %s in build.prop" % (prop,))
+  return None
 
 def AddToKnownPaths(filename, known_paths):
   if filename[-1] == "/":

sathish2026/manifest

Change-Id: I6b86ce389a2024e561bfd44ecece527ef0c51a38

tools/releasetools/ota_from_target_files.py
@@ -485,12 +485,23 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
     build = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
     date = GetBuildProp("ro.build.date", OPTIONS.info_dict)
-    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     flavour = GetBuildProp("ro.citrus.flavour", OPTIONS.info_dict)
     caf = GetBuildProp("ro.caf.branch", OPTIONS.info_dict)
 
+
+  if GetBuildProp("ro.product.model", OPTIONS.info_dict) is not None:
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     script.Print("***********************************************");
     script.Print("           Citrus-CAF for %s"%(model));
+    script.Print("     Flavour: %s"%(flavour));
+    script.Print("     CAF Revision: %s"%(caf));
+    script.Print("     AOSP Version: %s"%(build));
+    script.Print("     Compiled: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = GetBuildProp("ro.product.name", OPTIONS.info_dict)
+    script.Print("***********************************************");
+    script.Print("           Citrus-CAF for %s"%(name));
     script.Print("     Flavour: %s"%(flavour));
     script.Print("     CAF Revision: %s"%(caf));
     script.Print("     AOSP Version: %s"%(build));
@@ -577,7 +588,8 @@ def GetBuildProp(prop, info_dict):
   try:
     return info_dict.get("build.prop", {})[prop]
   except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
+    print ("WARNING: could not find %s in build.prop" % (prop,))
+  return None
 
 
 def HandleDowngradeMetadata(metadata):